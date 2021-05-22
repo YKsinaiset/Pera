2f015502bec08347645d135a6f2c855ddf5b94f5
liu <1624654897@qq.com>
2021-05-22 16:50:04 +0800

    hello

---
+++ build/CMakeFiles/Pera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pera.dir/helloworld.cpp.o"
+  "Pera.pdb"
+  "Pera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
